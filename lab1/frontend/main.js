7730f1888c07e2a0353b34bb9c105ccf87c11263
Ilasciuc Dmitrii <ilasciuc.dmitrii@mail.ru>
2020-11-25 21:42:18 +0200

    start work with lab 1; create simple electron app

---
+++ lab1/frontend/main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 500,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.loadFile('index.html');
+    win.webContents.openDevTools();
+}
+
+
+app.whenReady().then(createWindow);
